71506292497e9496b260c2faae07cfa2cc261a32
flrs <internetcop.de@web.de>
2017-12-17 17:21:13 -0700

    Updated package info

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.2',
+    version='1.0.3',
 
     description='Image processing blend modes',
     long_description=long_description,  #this is the
