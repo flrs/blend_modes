2eed764c2ac2691fc590b74b03595b272fc68770
flrs <internetcop.de@web.de>
2019-03-09 11:52:22 -0800

    Change test method to assertFalse for compatibility

--- tests/test_blend_modes.py
+++ tests/test_blend_modes.py
@@ -15,66 +15,67 @@ class TestBlendModes(unittest.TestCase):
     def test_addition(self):
         out = soft_light(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./soft_light.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_darken_only(self):
         out = darken_only(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./darken_only.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_difference(self):
         out = difference(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./difference.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_divide(self):
         out = divide(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./divide.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_dodge(self):
         out = dodge(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./dodge.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_grain_extract(self):
         out = grain_extract(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./grain_extract.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_grain_merge(self):
         out = grain_merge(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./grain_merge.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_hard_light(self):
         out = hard_light(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./hard_light.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_lighten_only(self):
         out = lighten_only(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./lighten_only.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_soft_light_50p(self):
         out = soft_light(self.img_in, self.img_layer_50p, 0.8)
         comp = cv2.imread('./soft_light_50p.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_overlay(self):
         out = overlay(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./overlay.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_normal_50p(self):
         out = normal(self.img_in, self.img_layer, 0.5)
         comp = cv2.imread('./normal_50p.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
 
     def test_normal_100p(self):
         out = normal(self.img_in, self.img_layer, 1.0)
         comp = cv2.imread('./normal_100p.png', -1).astype(float)
-        self.failIf(np.amax(np.absolute(out-comp)) > self.test_limit)
+        self.assertFalse(np.amax(np.absolute(out-comp)) > self.test_limit)
+
 
 unittest.main()
