20ff0f15eacb9efa9f8256876a24c88a7b962c01
flrs <internetcop.de@web.de>
2019-03-09 19:47:03 -0800

    Fix bug in setup.py where readme file cannot be found

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, './docs/README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='2.0.0',
+    version='2.0.1',
 
     description='Image processing blend modes',
     long_description=long_description,  #this is the
