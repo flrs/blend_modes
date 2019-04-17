4b21ce5fad6e3f713b89535a7851bdbfe35e81f8
flrs <internetcop.de@web.de>
2019-04-17 06:39:54 -0700

    Version bump to v2.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='2.0.1',
+    version='2.0.2',
 
     description='Image processing blend modes',
     long_description=long_description,
